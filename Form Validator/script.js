33daa3e71b5e1e7c8b6e5cba3ac81fe06738b817
Diego Aranguiz <daranguiz@theaustralgroup.com>
2020-08-12 04:00:03 -0400

    length, email and password match

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -18,38 +18,55 @@ function showSuccess(input){
     formControl.className = "form-control success";
 }
 
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(!re.test(String(input.value.trim()).toLowerCase())){
+        showError(input, "Email is not valid");
+    }else{
+        showSuccess(input);
+    };
+}
+
+//Check required fields
+function checkRequired(inputArray){
+    inputArray.forEach(function(item){
+        if(item.value === ""){
+            const message = item && item.name[0].toUpperCase()
+                + item.name.substring(1) + " is required";
+            showError(item, message);
+        }
+    });
+}
+
+// Check input length
+function checkInputLength(input, min, max){
+    if(input.value.length < min && input.value != ""){
+        showError(input, input.name[0].toUpperCase() + input.name.substring(1) + ` length should be more than ${min}`);
+    }else if(input.value.length > max && input.value != ""){
+        showError(input, input.name[0].toUpperCase() + input.name.substring(1) + ` length should be less than ${max}`);
+    }else if(input.value != ""){
+        showSuccess(input);
+    }
+}
+
+function checkPasswordsMatch(input1, input2){
+    if(input1.value === input2.value && input2.value != ""){
+        showSuccess(input1);
+        showSuccess(input2);
+    }else if(input2.value != ""){
+        showError(input2, "Passwords do not match");
+    }
 }
 
 //Event Listeners
 form.addEventListener("submit", function(e){
     e.preventDefault();
     
-    if(username.value === ""){
-        showError(username, "Username is required");
-    } else{
-        showSuccess(username)
-    }
-    if(email.value === ""){
-        showError(email, "Email is required");
-    }else if(!isValidEmail(email.value)){
-        showError(email, "Email is not valid");
-    }
-    else{
-        showSuccess(email)
-    }
-    if(password.value === ""){
-        showError(password, "Password is required");
-    } else{
-        showSuccess(password)
-    }
-    if(password2.value === ""){
-        showError(password2, "Password is required");
-    } else{
-        showSuccess(password2)
-    }
-
+    checkRequired([username, email, password, password2]);
+    checkPasswordsMatch(password, password2);
+    checkInputLength(username, 4, 16);
+    checkInputLength(password, 6, 16);  
+    checkEmail(email);  
+    
     
 });
